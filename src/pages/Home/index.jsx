cfda82ffecf0124aa4c14f28234d726234ef42a4
Mauricio <mauriciohillesheim15@gmail.com>
2024-09-13 19:58:29 -0300

    update

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css'
+
+export default function Home() {
+    return (
+        <div className='home-page'>
+            <h2>Venha você também descobrir o universo do Joke</h2>
+            <span>Para continuar, explore nossos conteúdos e descubra mais sobre o universo de Joke!</span>
+        </div>
+    )
+}

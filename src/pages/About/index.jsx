76728efb0137952846f2989a7dcf0a5e1eac7a59
Evertontech88 <evertonfarias1@live.com>
2024-09-13 20:04:32 -0300

    About

---
+++ src/pages/About/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css'
+
+export default function About() {
+    return (
+        <div className='about-page'>
+            <h2>Atualmente, o JokeAPI atende 1368 piadas de 6 idiomas diferentes.</h2>
+            <span>O JokeAPI está servindo muitas piadas ofensivas?</span>
+        </div>
+    )
+}

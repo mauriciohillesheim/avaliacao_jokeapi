5bb615091d04c3c89be5259fcebc846e0eb3b091
Evertontech88 <evertonfarias1@live.com>
2024-09-13 20:14:00 -0300

    Atualizado Joke API

---
+++ src/pages/JokeApi/index.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react'
+import './styles.css'
+import Card from '../../components/Card'
+
+export default function JokeApi() {
+    const [ conteudo, setConteudo ] = useState(<>Carregando...</>)
+
+    async function getCharacters() {
+        const reqOptions = {
+            method: "GET",
+            redirect: "follow"
+        }
+
+        const response = await fetch(
+            " https://v2.jokeapi.dev/joke/[Categoria/-s]",
+            reqOptions
+        )
+
+        if(!response.ok){
+            throw new Error("Http Error")
+        }
+
+        const apiResponse = await response.json()
+
+        return apiResponse
+    }
+
+    async function buildCards() {
+        const consulta = await getCharacters()
+        
+        return consulta.results.map(categoria => <Card data={categoria} />)
+    }
+
+    useEffect(() => {
+        async function getConteudo() {
+            setConteudo(await buildCards())
+        }
+
+        getConteudo()
+    }, [])
+
+    return (
+        <div className='list-api'>
+            { conteudo }
+        </div>
+    )   
+}

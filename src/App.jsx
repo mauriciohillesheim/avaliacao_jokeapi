264d3d4626acacb85a1865c36936cf54b14c56c9
Mauricio <mauriciohillesheim15@gmail.com>
2024-09-13 20:44:35 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,18 @@ import Header from './componets/Header'
 import Footer from './componets/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
- import JokeApi from './pages/JokeApi'
+import JokeApi from './pages/JokeApi'
  
- 
-
 export default function App() {
   return (
     <>
     <Header />
-
     <Routes> 
-
     <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/ joke-Api' element={<JokeApi />} />
-
     </Routes>
-
     <Footer/>
-    
-    </>
-  
-  )
+        </>
+    )
 }
-
-

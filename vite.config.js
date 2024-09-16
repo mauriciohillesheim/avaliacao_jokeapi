01db221c9bcf50397d41e9ba3eeb757af2806f1e
Mauricio <mauriciohillesheim15@gmail.com>
2024-09-16 19:48:59 -0300

    teste config vitejs

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/avaliacao_jokeapi/' // Adicione o subdiretório onde o projeto será hospedado
-})
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/avaliacao_jokeapi/' // Adicione o subdiretório onde o projeto será hospedado
+// })
 
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
 // // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+export default defineConfig({
+  plugins: [react()],
+})
